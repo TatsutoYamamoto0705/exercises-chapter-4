313f71aa4a4000d9f5ee14baaae0a302ff06f607
Yamamishiro <tatsutoymmt@gmail.com>
2022-08-13 05:39:44 +0100

    Fixed flake8 errors.

--- life/__init__.py
+++ life/__init__.py
@@ -1,1 +1,2 @@
-from .life import glider, blinker, glider_gun, Game
+from .life import glider, blinker, glider_gun, Game  # noqa F401
+"""Imports the corresponding modules into the life package namespace."""
